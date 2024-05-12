d09979cf6e801cd2fa98d596f54f504427660bfb
lorenzofoschi <91487500+ihcsof@users.noreply.github.com>
2024-05-12 16:50:31 +0200

    Add files via upload

--- mosaik_sim/worldWithOmnet.py
+++ mosaik_sim/worldWithOmnet.py
@@ -1,55 +1,61 @@
 from pathlib import Path
 import sys
-import mosaik
+from time import sleep
 
 base_path = Path(__file__).resolve().parent.parent.parent
 sys.path.insert(0, str(base_path))
 
-print("Python path after insertion:", sys.path)
-
-from cosima_core.util.util_functions import start_omnet, check_omnet_connection, stop_omnet, log
+from cosima_core.util.util_functions import start_omnet, \
+    check_omnet_connection, stop_omnet, \
+    log
 import cosima_core.util.general_config as cfg
+from scenario_config import NETWORK
+import mosaik
+import mosaik.util
 
-# Define the simulation end time and other constants
 SIMULATION_END = 260000
 START_MODE = 'cmd'
 NETWORK = 'SimpleNetworkTCP'
-ONTENT_PATH = cfg.ROOT_PATH / 'simulators' / 'tic_toc_example' / 'content.csv'
 
-# Simulation configuration
+# Simulation configuration -> tells mosaik where to find the simulators
 SIM_CONFIG = {
     'Simulator': {
         'python': 'SimulatorDiscreteMosaik:Simulator'
     },
-    'OmnetSimulator': {
-        'python': 'cosima_core.simulators.omnetpp_connection:OmnetppConnection'
+    'CommunicationSimulator': {
+        'python': 'cosima_core.simulators.communication_simulator:CommunicationSimulator',
     }
 }
 
-# Start the OMNeT++ process and check the connection
 omnet_process = start_omnet(START_MODE, NETWORK)
 check_omnet_connection(cfg.PORT)
 
-# Create the Mosaik world with the specified simulation configuration
+# Create mosaik World
 world = mosaik.World(SIM_CONFIG, time_resolution=0.001, cache=False)
 
-# Start the custom Simulator and the OMNeT++ Simulator
-sim = world.start('Simulator', eid_prefix='Model_')
-omnet_sim = world.start('OmnetSimulator', step_size=1, port=cfg.PORT, network=NETWORK).OmnetModel()
+client_attribute_mapping = {}
+for i in range(0, 51):
+    client_attribute_mapping[f'client{i}'] = f'message_with_delay_for_client{i}'
 
-# Instantiate multiple models within your simulator
+sim = world.start('Simulator', eid_prefix='Model_')
+
 models = sim.Prosumer.create(51, init_val=-1)  # Create 51 instances of Prosumer
 
-# Connect each Prosumer to the OMNeT++ simulator for data exchange
+comm_sim = world.start('CommunicationSimulator',
+                       step_size=1,
+                       port=cfg.PORT,
+                       client_attribute_mapping=client_attribute_mapping).CommunicationModel()
 for model in models:
-    world.connect(model, omnet_sim, ('data_to_omnet',))
-    world.connect(omnet_sim, model, ('data_from_omnet',))
+   world.connect(model, comm_sim, weak=True)
+   world.connect(comm_sim, model)
 
-# Run the simulation until the defined end time
-log(f'Running the simulation until {SIMULATION_END}')
+#mosaik.util.connect_many_to_one(world, models, comm_sim, 'src', 'dest', 'formatted_msg')
+
+# set initial event for simple agent
+world.set_initial_event(models[0].sid, time=0)
+
+log(f'run until {SIMULATION_END}')
 world.run(until=SIMULATION_END)
-log("End of simulation process")
-
-# Clean up after simulation
+log("end of process")
 sleep(5)
 stop_omnet(omnet_process)
