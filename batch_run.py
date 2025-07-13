fadcd16d89dff50e5c7a1a94e7f1bb5cb0bf63f7
ihcsof <lorenzo.foschi.work@gmail.com>
2025-07-13 15:02:59 +0000

    subgraphs

--- batch_run.py
+++ batch_run.py
@@ -106,7 +106,7 @@ def main():
 
     # Parameter sweeps
     methods             = ["method1", "method2"]
-    subgraph_nodes_list = [[0, 2, 3]]
+    subgraph_nodes_list = [list(range(i, i+3)) for i in range(0, 51, 3)]
     alphas              = [0.15, 0.5]  # only for method2
     byzantine_ids_list  = [[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22]]
     attack_probs        = [0.01, 0.1, 0.5]
